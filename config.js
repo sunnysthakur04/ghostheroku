aed7c839714e7b9b25dabf1b921b05983a204321
Sunny Thakur <sunnys.thakur@gmail.com>
2017-08-13 21:35:31 +0530

    Updating production URL config

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ config = {
     // ### Development **(default)**
     development: {
         // The url to use when providing links to the site, E.g. in RSS and email.
-        url: 'https://ghostheroku.herokuapp.com/',
+        url: 'http://my-ghost-blog.com',
 	mail: {}
         // Example mail config
         // Visit http://docs.ghost.org/mail for instructions
